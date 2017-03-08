93720ee812e147862de218181d43ca9da412fd99
hvelarde <hector.velarde@gmail.com>
2017-03-08 17:17:01 -0300

    Remove resource from legacy bundle

--- src/collective/lazysizes/upgrades/v7/__init__.py
+++ src/collective/lazysizes/upgrades/v7/__init__.py
@@ -8,11 +8,17 @@ JS = '++resource++collective.lazysizes/ls.respimg.min.js'
 
 
 def remove_respimg_polyfill(setup_tool):
-    """Use AMD version of Lazysizes."""
+    """Remove respimg polyfill plugin."""
     if IS_PLONE_5:
-        logger.warn('Upgrade step not supported under Plone 5')
-        return
+        record = 'plone.bundles/plone-legacy.resources'
+        resources = api.portal.get_registry_record(record)
+        id_ = 'resource-collective-lazysizes-ls-respimg-min-js'
+        if id_ in resources:
+            resources.remove(id_)
+            api.portal.set_registry_record(record, resources)
+            assert id_ not in api.portal.get_registry_record(record)
 
+    # we run this code in Plone 5 also "just in case"
     portal_js = api.portal.get_tool('portal_javascripts')
     if JS in portal_js.getResourceIds():
         portal_js.unregisterResource(JS)
